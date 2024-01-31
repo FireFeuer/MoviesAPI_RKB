6ab81fadeb74d9aa4dbf30f59916fa5a28ad1165
FireFeuer <92076566+FireFeuer@users.noreply.github.com>
2024-01-31 21:56:18 +0500

    v 0.2

--- IgnisFeuer/app/main.py
+++ IgnisFeuer/app/main.py
@@ -1,20 +1,21 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
-import requests
 import json
 import ctypes as ct
+
+
+
 
 API_URL_top = "http://192.168.138.222:5000/top"
 API_URL_movie = "http://192.168.138.222:5000/movie/"
 
 
+# НАЧАЛО РАБОТЫ С ГЛАВНЫМ ОКНОМ 
+
 root = tk.Tk()
 frm = ttk.Frame(root, padding=10)
-
-root.geometry('429x276+200+102')
-
-search_var = tk.StringVar(root)
+root.geometry('440x310+200+102')
 
 red_frame = Frame(root, bg="#CA1F3D")
 red_frame.place(x=0.0, y=0.3, relwidth=1.0, relheight=0.3)
@@ -22,21 +23,55 @@ red_frame.place(x=0.0, y=0.3, relwidth=1.0, relheight=0.3)
 black_frame = Frame(root, bg="#25182E")
 black_frame.place(x=0, rely=0.3, relwidth=1.0, relheight=0.7)
 
-label = tk.Label(root, text="Главная", bg = '#CA1F3D', fg='#fff', font='Helvetica 16 bold')
-label.grid(column=0, row=0, padx=20, pady=9)
+# КОНЕЦ РАБОТЫ С ГЛАВНЫМ ОКНОМ
 
-label = tk.Label(root, text="Рекомендации по фильму", bg = '#CA1F3D', fg='#fff', font='Helvetica 8')
-label.grid(row=1, column=0, padx=6, pady=2)
 
-button = tk.Button(root,
-                   font="Helvetica 10 bold",
-                   bg="yellow",
-                   activebackground="gold",
-                   relief="raised",
-                   )
-button.grid(column=1, row=0, padx=199)
+# НАЧАЛО РАБОТЫ С ЭЛЕМЕНТАМИ ГЛАВНОГО ОКНА 
 
-button.config(text="К жанрам", fg="white")
+label_Main = tk.Label(root, 
+                text="Главная", 
+                bg = '#CA1F3D', 
+                fg='#fff', 
+                font='Helvetica 16 bold')
+
+label_Main.grid(column=0, row=0, padx=6, pady=9,  sticky="w")
+
+label_Recommendation = tk.Label(root, 
+                    text="Рекомендации по фильму",  
+                    bg = '#CA1F3D', 
+                    fg='#fff', 
+                    font='Helvetica 9')
+
+label_Recommendation.grid(row=1, column=0, padx=9, pady=6, sticky="w")
+
+
+loadimage = tk.PhotoImage(file="button_k-zhanram.png")
+button_TransitionToGenres = tk.Button(root, image=loadimage)
+button_TransitionToGenres["bg"] = "#CA1F3D"
+button_TransitionToGenres["border"] = "0"
+button_TransitionToGenres.grid(column=1, row=0, padx=150)
+
+button_TransitionToGenres.config(text="К жанрам", fg="white")
+
+
+data = [
+("1", "Inception", "8.8", "star.jpg"),
+("2", "The Dark Knight", "9.0", "star.jpg"),
+("3", "Toy Story", "8.3", "star.jpg"),
+("4", "Interstellar", "8.6", "star.jpg"),
+("5", "Pirates of the Caribbean", "7.9", "star.jpg"),
+]
+
+tree = ttk.Treeview(show='', columns=('number', 'title', 'rating', 'icon'))
+tree.column('#0', width=0, stretch=NO)
+tree.column('number', anchor=CENTER, width=80)
+tree.column('title', anchor=CENTER, width=80)
+tree.column('rating', anchor=CENTER, width=80)
+tree.column('icon', anchor=CENTER, width=80)
+
+for item in data:
+    tree.insert('', 'end', text='Inception', values = item)
+tree.grid(column=0, row=2, padx=0, pady=20)
 
 
 root.mainloop()
@@ -60,12 +95,3 @@ def get_movie_info(movie_name):
     movies = json.loads(response.content.decode('utf-8'))
     return movies
 
-def dark_title_bar(window):
-    window.update()
-    set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
-    get_parent = ct.windll.user32.GetParent
-    hwnd = get_parent(window.winfo_id())
-    value = 2
-    value = ct.c_int(value)
-    set_window_attribute(hwnd, 20, ct.byref(value),
-                         4)
